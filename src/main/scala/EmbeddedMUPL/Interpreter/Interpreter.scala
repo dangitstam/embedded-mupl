d999b31bed3399d618bc10819beeff20f24ad87d
Tam Dang <dangitstam@D-10-18-199-248.dhcp4.washington.edu>
2019-01-09 10:06:31 -0800

    more two-space indentation

--- src/main/scala/EmbeddedMUPL/Interpreter/Interpreter.scala
+++ src/main/scala/EmbeddedMUPL/Interpreter/Interpreter.scala
@@ -5,128 +5,128 @@ import EmbeddedMUPL.Language.MUPL._
 
 object Interpreter {
 
-    /**
-     * Interprets the AST to a value (Const / Munit) beginning from an
-     * empty environment.
-     */
-    def eval(ast: Exp): Exp = evalUnderEnv(ast, List())
+  /**
+   * Interprets the AST to a value (Const / Munit) beginning from an
+   * empty environment.
+   */
+  def eval(ast: Exp): Exp = evalUnderEnv(ast, List())
 
-    private def envLookUp(variable: String, env: List[(String, Exp)]): Option[Exp] = {
-        env.find( _._1 == variable ) match {
-            case Some((s, v)) => Some(v)
-            case _          => None
+  private def envLookUp(variable: String, env: List[(String, Exp)]): Option[Exp] = {
+    env.find( _._1 == variable ) match {
+      case Some((s, v)) => Some(v)
+      case _      => None
+    }
+  }
+
+  @throws(classOf[ArithmeticException])
+  private def evalUnderEnv(ast: Exp, env: List[(String, Exp)]): Exp = ast match {
+    case Var(s) => envLookUp(s, env) match {
+      case Some(e) => evalUnderEnv(e, env)
+      case None => throw new BadMUPLExpression("Undefined variable %s".format(s))
+    }
+    case Munit() => ast
+    case Const(i) => ast
+
+    // Arithmetic operations.
+    case Add(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
+      case (Const(i), Const(j)) => Const(i + j)
+      case _  => throw new BadMUPLExpression("Addition includes non-Const")
+    }
+    case Subtract(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
+      case (Const(i), Const(j)) => Const(i - j)
+      case _  => throw new BadMUPLExpression("Subtraction includes non-Const")
+    }
+    case Multiply(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
+      case (Const(i), Const(j)) => Const(i * j)
+      case _  => throw new BadMUPLExpression("Multiplication includes non-Const")
+    }
+    case Divide(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
+      case (Const(i), Const(j)) =>
+        if (j > 0) {
+          Const(i / j)
+        } else {
+          throw new BadMUPLExpression("Division by zero")
         }
+      case _  => throw new BadMUPLExpression("Multiplication includes non-Const")
     }
 
-    @throws(classOf[ArithmeticException])
-    private def evalUnderEnv(ast: Exp, env: List[(String, Exp)]): Exp = ast match {
-        case Var(s) => envLookUp(s, env) match {
-            case Some(e) => evalUnderEnv(e, env)
-            case None => throw new BadMUPLExpression("Undefined variable %s".format(s))
-        }
-        case Munit() => ast
-        case Const(i) => ast
-
-        // Arithmetic operations.
-        case Add(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
-            case (Const(i), Const(j)) => Const(i + j)
-            case _  => throw new BadMUPLExpression("Addition includes non-Const")
-        }
-        case Subtract(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
-            case (Const(i), Const(j)) => Const(i - j)
-            case _  => throw new BadMUPLExpression("Subtraction includes non-Const")
-        }
-        case Multiply(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
-            case (Const(i), Const(j)) => Const(i * j)
-            case _  => throw new BadMUPLExpression("Multiplication includes non-Const")
-        }
-        case Divide(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
-            case (Const(i), Const(j)) =>
-                if (j > 0) {
-                    Const(i / j)
-                } else {
-                    throw new BadMUPLExpression("Division by zero")
-                }
-            case _  => throw new BadMUPLExpression("Multiplication includes non-Const")
-        }
-
-        // Pair & list semantics.
-        case Apair(e1, e2) => Apair(evalUnderEnv(e1, env), evalUnderEnv(e2, env))
-        case First(e) => evalUnderEnv(e, env) match {
-            case Apair(e1, e2) => e1
-            case _ => throw new BadMUPLExpression("First applied to non-pair")
-        }
-        case Second(e) => evalUnderEnv(e, env) match {
-            case Apair(e1, e2) => e2
-            case _ => throw new BadMUPLExpression("Second applied to non-pair")
-        }
-
-        // Comparisons and control flow.
-        case IsMunit(e) => evalUnderEnv(e, env) match {
-            case Munit() => Const(1)
-            case _       => Const(0)
-        }
-        case IsGreater(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
-            case (Const(i), Const(j)) =>
-                if (i > j) {
-                    Const(1)
-                } else {
-                    Const(0)
-                }
-            case _  => throw new BadMUPLExpression("isgreater applied to non-Const")
-        }
-        case Ifnz(cond, e1, e2) => evalUnderEnv(cond, env) match {
-            case Const(i) =>
-                if (i != 0) {
-                    evalUnderEnv(e1, env)
-                } else {
-                    evalUnderEnv(e2, env)
-                }
-            case _  => throw new BadMUPLExpression("ifnz applied to non-Const")
-        }
-
-        // Variable bindings.
-        case Let(name: Var, value: Exp, body: Exp) => {
-            val Var(s) = name;
-            evalUnderEnv(body, (s, evalUnderEnv(value, env)) :: env)
-        }
-        
-        // Function call semantics.
-        case Fun(fname, argname: Var, body: Exp) => {
-            Closure(env, Fun(fname, argname, body))
-        }
-        case Closure(env, fn) => Closure(env, fn)
-        case Call(fn: Exp, arg: Exp) => evalUnderEnv(fn, env) match {
-            case Closure(fnEnv, fn) => {
-                val Fun(fnNameVariable, Var(fnArgName), fnBody) = fn
-
-                // Create a mapping for the evaluated function argument.
-                var bodyEnv = (fnArgName, evalUnderEnv(arg, env)) :: fnEnv
-
-                // Mapping named functionf in the environment allows recursion.
-                if (fnNameVariable != null) {
-                    val Var(fnName) = fnNameVariable
-                    bodyEnv = (fnName, Closure(fnEnv, fn)) :: bodyEnv
-                }
-
-                evalUnderEnv(fnBody, bodyEnv)
-            }
-            case _ => {
-                throw new BadMUPLExpression("Function call applied with non-function")
-            }
-        }
+    // Pair & list semantics.
+    case Apair(e1, e2) => Apair(evalUnderEnv(e1, env), evalUnderEnv(e2, env))
+    case First(e) => evalUnderEnv(e, env) match {
+      case Apair(e1, e2) => e1
+      case _ => throw new BadMUPLExpression("First applied to non-pair")
+    }
+    case Second(e) => evalUnderEnv(e, env) match {
+      case Apair(e1, e2) => e2
+      case _ => throw new BadMUPLExpression("Second applied to non-pair")
     }
 
-    /**
-     * Custom exceptions.
-     */
-    final case class DivisionByZero(
-        private val message: String = "Division by zero", 
-        private val cause: Throwable = None.orNull)
-        extends Exception(message, cause)
+    // Comparisons and control flow.
+    case IsMunit(e) => evalUnderEnv(e, env) match {
+      case Munit() => Const(1)
+      case _     => Const(0)
+    }
+    case IsGreater(a, b) => (evalUnderEnv(a, env), evalUnderEnv(b, env)) match {
+      case (Const(i), Const(j)) =>
+        if (i > j) {
+          Const(1)
+        } else {
+          Const(0)
+        }
+      case _  => throw new BadMUPLExpression("isgreater applied to non-Const")
+    }
+    case Ifnz(cond, e1, e2) => evalUnderEnv(cond, env) match {
+      case Const(i) =>
+        if (i != 0) {
+          evalUnderEnv(e1, env)
+        } else {
+          evalUnderEnv(e2, env)
+        }
+      case _  => throw new BadMUPLExpression("ifnz applied to non-Const")
+    }
 
-    final case class BadMUPLExpression(
-        private val message: String = "", 
-        private val cause: Throwable = None.orNull)
-        extends Exception(message, cause)
+    // Variable bindings.
+    case Let(name: Var, value: Exp, body: Exp) => {
+      val Var(s) = name;
+      evalUnderEnv(body, (s, evalUnderEnv(value, env)) :: env)
+    }
+    
+    // Function call semantics.
+    case Fun(fname, argname: Var, body: Exp) => {
+      Closure(env, Fun(fname, argname, body))
+    }
+    case Closure(env, fn) => Closure(env, fn)
+    case Call(fn: Exp, arg: Exp) => evalUnderEnv(fn, env) match {
+      case Closure(fnEnv, fn) => {
+        val Fun(fnNameVariable, Var(fnArgName), fnBody) = fn
+
+        // Create a mapping for the evaluated function argument.
+        var bodyEnv = (fnArgName, evalUnderEnv(arg, env)) :: fnEnv
+
+        // Mapping named functionf in the environment allows recursion.
+        if (fnNameVariable != null) {
+          val Var(fnName) = fnNameVariable
+          bodyEnv = (fnName, Closure(fnEnv, fn)) :: bodyEnv
+        }
+
+        evalUnderEnv(fnBody, bodyEnv)
+      }
+      case _ => {
+        throw new BadMUPLExpression("Function call applied with non-function")
+      }
+    }
+  }
+
+  /**
+   * Custom exceptions.
+   */
+  final case class DivisionByZero(
+    private val message: String = "Division by zero", 
+    private val cause: Throwable = None.orNull)
+    extends Exception(message, cause)
+
+  final case class BadMUPLExpression(
+    private val message: String = "", 
+    private val cause: Throwable = None.orNull)
+    extends Exception(message, cause)
 }
--- src/main/scala/EmbeddedMUPL/Language/MUPL.scala
+++ src/main/scala/EmbeddedMUPL/Language/MUPL.scala
@@ -7,40 +7,40 @@ import java.lang.ArithmeticException
 
 sealed trait Exp {
 
-    /**
-     * Defines basic portions of the DSL syntax.
-     * Equates infix operators to their AST equivalents.
-     */
-    def plus(other: Exp) = Add(this, other)
-    def minus(other: Exp) = Subtract(this, other)
-    def times(other: Exp) = Multiply(this, other)
-    def divide(other: Exp) = Divide(this, other)
+  /**
+   * Defines basic portions of the DSL syntax.
+   * Equates infix operators to their AST equivalents.
+   */
+  def plus(other: Exp) = Add(this, other)
+  def minus(other: Exp) = Subtract(this, other)
+  def times(other: Exp) = Multiply(this, other)
+  def divide(other: Exp) = Divide(this, other)
 
-    /**
-     * Control flow.
-     *
-     * Custom if-not-zero:
-     * e1 ifnz cond otherwise e2
-     */
-    def ifnz(cond: Exp) = new Otherwise(cond, this)
+  /**
+   * Control flow.
+   *
+   * Custom if-not-zero:
+   * e1 ifnz cond otherwise e2
+   */
+  def ifnz(cond: Exp) = new Otherwise(cond, this)
 }
 
 /**
  * Original MUPL constructs.
  */
-final case class Var(s: String)                            extends Exp
-final case class Const(i: Int)                             extends Exp
-final case class Add(e1: Exp, e2: Exp)                     extends Exp
-final case class IsGreater(e1: Exp, e2: Exp)               extends Exp
-final case class Ifnz(e1: Exp, e2: Exp, e3: Exp)           extends Exp
-final case class Call(e1: Exp, e2: Exp)                    extends Exp
-final case class Fun(s1: Var, s2: Var, e: Exp)             extends Exp
-final case class Let(name: Var, value: Exp, body: Exp)     extends Exp
-final case class Apair(e1: Exp, e2: Exp)                   extends Exp
-final case class First(e: Exp)                             extends Exp
-final case class Second(e: Exp)                            extends Exp
-final case class Munit()                                   extends Exp
-final case class IsMunit(e: Exp)                           extends Exp
+final case class Var(s: String)              extends Exp
+final case class Const(i: Int)               extends Exp
+final case class Add(e1: Exp, e2: Exp)           extends Exp
+final case class IsGreater(e1: Exp, e2: Exp)         extends Exp
+final case class Ifnz(e1: Exp, e2: Exp, e3: Exp)       extends Exp
+final case class Call(e1: Exp, e2: Exp)          extends Exp
+final case class Fun(s1: Var, s2: Var, e: Exp)       extends Exp
+final case class Let(name: Var, value: Exp, body: Exp)   extends Exp
+final case class Apair(e1: Exp, e2: Exp)           extends Exp
+final case class First(e: Exp)               extends Exp
+final case class Second(e: Exp)              extends Exp
+final case class Munit()                   extends Exp
+final case class IsMunit(e: Exp)               extends Exp
 final case class Closure(env: List[(String, Exp)], f: Fun) extends Exp
 
 /**
@@ -52,7 +52,7 @@ final case class Divide(a: Exp, b: Exp) extends Exp
 
 // For use in ifnz only:
 class Otherwise(cond: Exp, e1: Exp) {
-    def otherwise(e2: Exp) = new Ifnz(cond, e1, e2)
+  def otherwise(e2: Exp) = new Ifnz(cond, e1, e2)
 }
 
 /**
@@ -61,72 +61,72 @@ class Otherwise(cond: Exp, e1: Exp) {
  */
 object Enhancements {
 
-     /** 
-      * The presence of these implicit definitions allow
-      * using strings and ints in place of Var(string)
-      * and Const(int).
-      *
-      * For instance, "x" plus "y" is implicitly converted to
-      * Var("x") plus Var("y") when plus is called.
-      */
-    implicit def StringToExp(s: String): Var = Var(s)
-    implicit def intToExp(i: Int): Const = Const(i)
+   /** 
+    * The presence of these implicit definitions allow
+    * using strings and ints in place of Var(string)
+    * and Const(int).
+    *
+    * For instance, "x" plus "y" is implicitly converted to
+    * Var("x") plus Var("y") when plus is called.
+    */
+  implicit def StringToExp(s: String): Var = Var(s)
+  implicit def intToExp(i: Int): Const = Const(i)
 
-    /**
-     * Comparisons.
-     */
-    def isgreater(e1: Exp, e2: Exp) = IsGreater(e1, e2)
+  /**
+   * Comparisons.
+   */
+  def isgreater(e1: Exp, e2: Exp) = IsGreater(e1, e2)
 
-    /**
-     * Custom let expresion:
-     * let variable "x" equal ("x" plus "y") in ("z" minus 14 divide "z")
-     *
-     * Custom function definition:
-     * define function null of "x" as (
-     *     "x" times 17
-     * )
-     */ 
-    def let = new LetConstructor()
-    class LetConstructor() {
-        def variable(name: Var): Eq = new Eq(name)
+  /**
+   * Custom let expresion:
+   * let variable "x" equal ("x" plus "y") in ("z" minus 14 divide "z")
+   *
+   * Custom function definition:
+   * define function null of "x" as (
+   *   "x" times 17
+   * )
+   */ 
+  def let = new LetConstructor()
+  class LetConstructor() {
+    def variable(name: Var): Eq = new Eq(name)
+  }
+  def define = new FunctionConstructor()
+  class FunctionConstructor() {
+    def function(name: Var): Eq = new Eq(name)
+  }
+  class Eq(name: Var) {
+    def equal(value: Exp) = new In(name, value)
+    def of(arg: Var) = new As(name, arg)
+  }
+  class In(name: Var, value: Exp) { def in(body: Exp): Exp = Let(name, value, body) }
+  class As(name: Var, arg:   Var) { def as(body: Exp): Exp = Fun(name, arg, body) }
+
+  /**
+   * Custom function call:
+   * apply function "f" on ("x" plus 12)
+   */
+  def apply = new CallConstructor()
+  class CallConstructor() { def function(fname: Exp): Use = new Use(fname) }
+  class Use(fname: Exp)     { def on(arg: Exp) = new Call(fname, arg) }
+
+  /**
+   * Alternative if-not-zero:
+   * ifnz(cond) then e1 else e2 
+   */
+   def ifnz(cond: Exp) = new IfnzConstructor(cond)
+   class IfnzConstructor(cond: Exp) {
+     def then(e1: Exp) = new Otherwise(cond, e1)
+   }
+
+   /**
+    * List construction and semantics.
+    * apair e1 e2
+    */
+    def construct = new CollectionsConstructor()
+    class CollectionsConstructor() {
+      def pair(e1: Exp, e2: Exp) = Apair(e1, e2)
     }
-    def define = new FunctionConstructor()
-    class FunctionConstructor() {
-        def function(name: Var): Eq = new Eq(name)
-    }
-    class Eq(name: Var) {
-        def equal(value: Exp) = new In(name, value)
-        def of(arg: Var) = new As(name, arg)
-    }
-    class In(name: Var, value: Exp) { def in(body: Exp): Exp = Let(name, value, body) }
-    class As(name: Var, arg:   Var) { def as(body: Exp): Exp = Fun(name, arg, body) }
-
-    /**
-     * Custom function call:
-     * apply function "f" on ("x" plus 12)
-     */
-    def apply = new CallConstructor()
-    class CallConstructor() { def function(fname: Exp): Use = new Use(fname) }
-    class Use(fname: Exp)       { def on(arg: Exp) = new Call(fname, arg) }
-
-    /**
-     * Alternative if-not-zero:
-     * ifnz(cond) then e1 else e2 
-     */
-     def ifnz(cond: Exp) = new IfnzConstructor(cond)
-     class IfnzConstructor(cond: Exp) {
-         def then(e1: Exp) = new Otherwise(cond, e1)
-     }
-
-     /**
-      * List construction and semantics.
-      * apair e1 e2
-      */
-      def construct = new CollectionsConstructor()
-      class CollectionsConstructor() {
-          def pair(e1: Exp, e2: Exp) = Apair(e1, e2)
-      }
-      def first(e: Exp) = First(e)
-      def second(e: Exp) = Second(e)
-      def ismunit(e: Exp) = IsMunit(e)
+    def first(e: Exp) = First(e)
+    def second(e: Exp) = Second(e)
+    def ismunit(e: Exp) = IsMunit(e)
 }
